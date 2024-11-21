108f7e551d6993fd6dcdf43bee6fe7c0e53fd0e1
jkweber236 <jkweber236@gmail.com>
2024-11-20 21:51:06 -0700

    Troubleshooting

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app
+    .use(bodyParser.json())
+    .use('/', require('./routes'))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+    
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
 });
