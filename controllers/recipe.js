8f40cfaaac2027cf82edf632ea24d78a8ec5e966
jkweber236 <jkweber236@gmail.com>
2024-11-21 19:53:38 -0700

    Populated recipes collection and added get functionaltiy

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,7 +1,21 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllRecipes = async (/*req, res*/) => {};
+const getAllRecipes = async (req, res) => {
+    mongodb
+    .getDb()
+    .db('MealPlanner')
+    .collection('recipes')
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  };
+
 const getRecipe = async (/*req, res*/) => {};
 const createRecipe = async (/*req, res*/) => {};
 const updateRecipe = async (/*req, res*/) => {};
