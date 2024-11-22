3b339cfe35606e3aef173f821afb1e5dedb7ca59
dalanweber9876 <dalanweber9876@gmail.com>
2024-11-22 02:34:15 -0700

    Updated getAllRecipes controller

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,23 +2,19 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllRecipes = async (req, res) => {
-    await mongodb
-    .getDb()
-    .db('MealPlanner')
-    .collection('recipes')
-    .find()
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-  };
+  try {
+    const lists = await mongodb.getDb().db('MealPlanner').collection('recipes').find().toArray();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 const getRecipe = async (/*req, res*/) => {};
 const createRecipe = async (/*req, res*/) => {};
 const updateRecipe = async (/*req, res*/) => {};
 const deleteRecipe = async (/*req, res*/) => {};
 
-module.exports = { getAllRecipes, getRecipe, createRecipe, updateRecipe, deleteRecipe }
+module.exports = { getAllRecipes, getRecipe, createRecipe, updateRecipe, deleteRecipe };
