66127c5fc5080e22769fce9450c621e649b7c57d
jkweber236 <jkweber236@gmail.com>
2024-11-26 01:33:12 -0700

    Adding to meal plans

--- controllers/mealPlan.js
+++ controllers/mealPlan.js
@@ -1,7 +1,15 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getMealPlan = async (/*req, res*/) => {};
+const getMealPlan = async (req, res) => {
+    try {
+        const mealPlanId = new ObjectId(req.params.id);
+        const response = await mongodb.getDb().db('MealPlanner').collection('meal-plan').find({ _id: mealPlanId }).toArray();
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
 const createMealPlan = async (/*req, res*/) => {};
 const updateMealPlan = async (/*req, res*/) => {};
 const deleteMealPlan = async (/*req, res*/) => {};
